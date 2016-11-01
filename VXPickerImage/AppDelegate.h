20d722c143388bb7890c8276fe3767a92a624aa2
voidxin <zhangxin409999@163.com>
2016-11-01 14:05:41 +0800

    Initial Commit

---
+++ VXPickerImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VXPickerImage
+//
+//  Created by voidxin on 16/11/1.
+//  Copyright © 2016年 wugumofang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VXPickerImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VXPickerImage
+//
+//  Created by voidxin on 16/11/1.
+//  Copyright © 2016年 wugumofang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
